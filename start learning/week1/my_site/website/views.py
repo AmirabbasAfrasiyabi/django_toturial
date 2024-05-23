385cdec49822f1268884cc90da7038f84a0c4943
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-05-23 11:49:35 +0330

    created reander Html file

--- start learning/week1/my_site/website/views.py
+++ start learning/week1/my_site/website/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse 
+
 def Index_view(request):
-    return render(request,'website/index.html')
+    return render(request,'index.html')
 
 def About_view(request):
-    return render(request,'website/index.html')
+    return render(request,'about.html')
 
 def Contact_view(request):
-    return render(request,'about.html')
+    return render(request,'contact.html')
