ff12d76e867813fbee997e9f86f10b2d091986cc
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-05-25 16:52:00 +0330

    update

--- start learning/week1/my_site/my_site/settings.py
+++ start learning/week1/my_site/my_site/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'website.apps.WebsiteConfig',
-    'website'
+    'website',
+    'blog',
 ]
 
 MIDDLEWARE = [
