6b81d9633f9879c5a3b31569428717052fe03eb9
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-05-27 18:03:34 +0330

    updated  website models and created admin panel for websites app

--- start learning/week1/my_site/website/models.py
+++ start learning/week1/my_site/website/models.py
@@ -9,3 +9,10 @@ class contact (models.Model):
    massage = models.TextField()
    created_date = models.DateTimeField(auto_now_add=True)
    update_date = models.DateTimeField(auto_now=True)
+
+   class Meta:
+      ordering = ('created_date',)
+
+   def __str__(self):
+      return self.name
+   
