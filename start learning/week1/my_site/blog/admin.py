c7e57ed35cf43672802253ea478143f96b5089d8
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-05-27 17:16:42 +0330

    updated admin for paneladmin

--- start learning/week1/my_site/blog/admin.py
+++ start learning/week1/my_site/blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+
+##second way
+# @admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    
+    date_hierarchy = "created_date"
+    empty_value_display = "-empty-"
+    # fields = ["title",]
+    list_display = ["title" , "count_views" , "status" ,]
+    list_filter = ('status',)
+    ordering = ['-created_date']
+    search_fields = ['title','content']
+##first way
+admin.site.register(Post,PostAdmin)
+
