7c9032872c4d1e6392b383df76acafa705d50823
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-05-28 15:26:57 +0330

    complated  blog post with the following message

--- start learning/week1/my_site/blog/views.py
+++ start learning/week1/my_site/blog/views.py
@@ -6,11 +6,13 @@ def blog_view (request):
     context = {'posts':posts}
     return render (request , 'blog/blog-home.html',context)
 
-def blog_single (request):
-    return render (request , 'blog/blog-single.html')
+def blog_single (request,pid):
+    posts = Post.objects.filter(status=1)
+    post = get_object_or_404(posts,pk=pid)
+    context = {'post':post}    
+    return render (request , 'blog/blog-single.html',context)
 
 def test (request,pid):
     post = get_object_or_404(Post,pk=pid)
-    # post = Post.objects.get(id=pid)
     context = {'post':post}    
     return render (request ,'test.html',context)
