db4c6a8865e05b98ae3d8991532c85b5425af043
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-05-28 09:20:04 +0330

    update blog-home.html  for more information

--- start learning/week1/my_site/blog/views.py
+++ start learning/week1/my_site/blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from blog.models import Post
 # Create your views here.
 def blog_view (request):
-    return render (request , 'blog/blog-home.html')
+    Posts = Post.objects.filter(status=1)
+    context = {'Posts':Posts}
+    return render (request , 'blog/blog-home.html',context)
 
 def blog_single (request):
     return render (request , 'blog/blog-single.html')
