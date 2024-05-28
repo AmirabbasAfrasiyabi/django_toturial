c51937b62e36f53b7434a534bf15ba752bbecc46
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-05-28 14:53:27 +0330

    update anad repaire code

--- start learning/week1/my_site/blog/views.py
+++ start learning/week1/my_site/blog/views.py
@@ -1,17 +1,16 @@
-from django.shortcuts import render , get_object_or_404
+from django.shortcuts import render ,get_object_or_404
 from blog.models import Post
 # Create your views here.
 def blog_view (request):
-    Posts = Post.objects.filter(status=1)
-    context = {'Posts':Posts}
+    posts = Post.objects.filter(status=1)
+    context = {'posts':posts}
     return render (request , 'blog/blog-home.html',context)
 
 def blog_single (request):
     return render (request , 'blog/blog-single.html')
 
 def test (request,pid):
-    #Posts = Post.objects.get(id = pid)
-    Posts = get_object_or_404(Post, pk=pid)
-    context = {'Posts':Posts}    
-    # context = {'pid':pid}
-    return render (request , 'test.html',context)
+    post = get_object_or_404(Post,pk=pid)
+    # post = Post.objects.get(id=pid)
+    context = {'post':post}    
+    return render (request ,'test.html',context)
