fecda0a82438f25bb3916ed5583858d64c6cea20
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-05-28 10:19:18 +0330

    created urls parameter and blog tags

--- start learning/week1/my_site/blog/views.py
+++ start learning/week1/my_site/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from blog.models import Post
 # Create your views here.
 def blog_view (request):
@@ -9,7 +9,9 @@ def blog_view (request):
 def blog_single (request):
     return render (request , 'blog/blog-single.html')
 
-def test (request):
-    Posts = Post.objects.all()
-    context = {'Posts':Posts}
+def test (request,pid):
+    #Posts = Post.objects.get(id = pid)
+    Posts = get_object_or_404(Post, pk=pid)
+    context = {'Posts':Posts}    
+    # context = {'pid':pid}
     return render (request , 'test.html',context)
