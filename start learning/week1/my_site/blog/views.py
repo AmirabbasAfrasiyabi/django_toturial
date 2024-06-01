94fd2b5e81fee5e1a994d416eaa480275b7556e0
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-06-01 13:15:39 +0330

    use simple tags

--- start learning/week1/my_site/blog/views.py
+++ start learning/week1/my_site/blog/views.py
@@ -12,7 +12,15 @@ def blog_single (request,pid):
     context = {'post':post }    
     return render (request , 'blog/blog-single.html',context)
 
-def test (request,pid):
-    post = get_object_or_404(Post,pk=pid)
-    context = {'post':post}    
-    return render (request ,'test.html',context)
+# def test (request.pid):
+    # post = get_object_or_404(Post,pk=pid)
+    # context = {'post':post}    
+    # return render (request ,'test.html',context)
+
+
+def test (request):
+    # post = get_object_or_404(Post,pk=pid)
+    # context = {'post':post}    
+    # return render (request ,'test.html',context)
+
+    return render (request ,'test.html')
