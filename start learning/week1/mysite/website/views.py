fb691a0b1c1fe4a582bda61954026e0cddfb21f2
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-06-08 11:36:28 +0330

    created render html files

--- start learning/week1/mysite/website/views.py
+++ start learning/week1/mysite/website/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse , JsonResponse
 def index_view(request):
-    return HttpResponse('<h1>Home page</h1>')
+    return render(request,'website/index.html')
 
 def about_view(request):
-    return HttpResponse('<h1>Welcome</h1>')
+    return render(request,'website/about.html')
 
 def Contact_view(request):
-    return HttpResponse('<h1>Welcome</h1>')
+    return render(request,'website/Contact.html')
