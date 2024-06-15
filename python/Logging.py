7a610b02705de807ad24e3a70b22a6459db56ad2
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-06-15 10:49:59 +0330

    logger

--- python/Logging.py
+++ python/Logging.py
@@ -23,12 +23,18 @@
 
 ## example 2
 
+# import logging
+
+# a = 5
+# b = 0
+
+# try:
+#   c = a / b
+# except Exception as e:
+#   logging.error("Exception occurred", exc_info=True)
+
+
+## example 3
 import logging
-
-a = 5
-b = 0
-
-try:
-  c = a / b
-except Exception as e:
-  logging.error("Exception occurred", exc_info=True)
+logger = logging.getLogger('example_logger')
+logger.warning('This is a warning')
