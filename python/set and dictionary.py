87b61c707164eaded4e679853860714abce6fd36
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-06-15 17:19:14 +0330

    set and dict created

---
+++ python/set and dictionary.py
@@ -0,0 +1,18 @@
+list_t = {1,2,3,4,5}
+set_t = set(list_t)
+set_t.add(6)
+set_t.remove(3)
+print(list_t,set_t)
+
+
+set1 = {1,2,3,4,5}
+set2 = {4,5,6,7,8}
+print(set1 | set2)
+print(set1 & set2)
+print(set1 - set2)
+
+
+#dictionary
+
+dict ={'name':'ali', 'last_name':'afrasiyabi'}
+print(dict['name'])
