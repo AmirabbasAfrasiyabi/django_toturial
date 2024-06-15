62895bff9d23875a35a6997477312cd9dad63bb3
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-06-15 17:03:04 +0330

    update

--- python/time.py
+++ python/time.py
@@ -5,3 +5,9 @@ from datetime import datetime
 datetime_object = datetime.now()
 print(datetime_object)
 print('Type :- ',type(datetime_object))
+my_string = '2021-01-24'
+
+# Create date object in given time format yyyy-mm-dd
+my_date = datetime.strptime(my_string, "%Y-%m-%d")
+print(my_date)
+print('Type: ',type(my_date))
