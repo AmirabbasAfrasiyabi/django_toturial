c5d89c7050bf88cd741cbe92cd52739c67633128
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-06-15 10:46:56 +0330

    try - exept Excwption as e Error

--- python/Logging.py
+++ python/Logging.py
@@ -1,22 +1,34 @@
+# import logging
+# #level
+# logging.basicConfig(level=logging.DEBUG)
+# logging.debug('This will get logged')
+
+
+# #filemode
+# logging.basicConfig(filename='app.log', filemode='w')
+# logging.warning('This will get logged to a file')
+
+
+# #format
+# logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+# logging.warning('This is a warning message')
+
+
+# logging.debug('This is a debug message')
+# logging.info('This is an info message')
+# logging.warning('This is a warning message')
+# logging.error('This is an error message')
+# logging.critical('This is a critical message')
+
+
+## example 2
+
 import logging
-#level
-logging.basicConfig(level=logging.DEBUG)
-logging.debug('This will get logged')
 
+a = 5
+b = 0
 
-#filemode
-logging.basicConfig(filename='app.log', filemode='w')
-logging.warning('This will get logged to a file')
-
-
-#format
-
-logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-logging.warning('This is a warning message')
-
-
-logging.debug('This is a debug message')
-logging.info('This is an info message')
-logging.warning('This is a warning message')
-logging.error('This is an error message')
-logging.critical('This is a critical message')
+try:
+  c = a / b
+except Exception as e:
+  logging.error("Exception occurred", exc_info=True)
