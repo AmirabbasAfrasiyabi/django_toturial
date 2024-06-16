c7acc477b0297eb678b8507be30875755470e139
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-06-16 10:05:42 +0330

    update

--- python/set and dictionary.py
+++ python/set and dictionary.py
@@ -1,6 +1,8 @@
-list_t = {1,2,3,4,5}
+list_t = [1,2,3,4,4,5,5,5]
 set_t = set(list_t)
+print(list_t,set_t)
 set_t.add(6)
+set_t.add(7)
 set_t.remove(3)
 print(list_t,set_t)
 
@@ -10,9 +12,15 @@ set2 = {4,5,6,7,8}
 print(set1 | set2)
 print(set1 & set2)
 print(set1 - set2)
-
+print(set2 - set1)
+print(set1 ^ set2)
 
 #dictionary
 
 dict ={'name':'ali', 'last_name':'afrasiyabi'}
+dict['name'] = 'amirabbas'
 print(dict['name'])
+print(dict.get('last_name')) 
+dict.update({'name':'amirabbas', 'last_name':'mohamadi'})
+print(dict['name'])
+print(dict.get('last_name')) 
