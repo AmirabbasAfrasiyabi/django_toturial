4eec5ca7c0cb77822dedfa66e8766643096b4d17
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-06-16 10:20:41 +0330

    function createdkwargs)

---
+++ python/functions.py
@@ -0,0 +1,20 @@
+def print_hello_world():
+    for i in range(3):
+        print("Hello World")
+
+print_hello_world()
+
+def aca():
+    print("done")
+
+def cs():
+    return 3
+
+print(aca())
+print(cs())
+
+## args kwarg
+def test(**kwargs):
+    print(kwargs)
+test(name="amir")
+
