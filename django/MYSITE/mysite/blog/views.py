cf0510c849946823423a2139075075a926829adc
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-06-26 15:41:51 +0330

    work with blog single app

--- django/MYSITE/mysite/blog/views.py
+++ django/MYSITE/mysite/blog/views.py
@@ -5,4 +5,5 @@ def blog_view (request):
     return render(request, 'blog/blog-home.html')
 
 def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+    context = {'title':'Bitcoin crashed againd','content':'Bitcoin was finally recovered','author':'amirabbas'}
+    return render(request, 'blog/blog-single.html',context)
