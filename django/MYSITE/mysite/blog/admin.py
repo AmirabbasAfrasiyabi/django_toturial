512405cd5063b7d26fcbed1658521068642639d9
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-06-27 21:55:41 +0330

    work with admin for blog and website app

--- django/MYSITE/mysite/blog/admin.py
+++ django/MYSITE/mysite/blog/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    empty_value_display = '-empty-'
+    # fields = ("title","content","published_date", "status", "created_date" , "updated_date" )
+    list_display = ("title","content","published_date", "status", "created_date" , "updated_date" )
+    list_filter = ("status",)
+    # ordering = ['-created_date']
+    search_fields = ['title','content']
+admin.site.register(Post,PostAdmin)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
--- django/MYSITE/mysite/website/admin.py
+++ django/MYSITE/mysite/website/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from website.models import Contact
 
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    empty_value_display = '-empty-'
+    # fields = ("name","email","subject","massage","created_date") 
+    list_display = ("name","email","subject","created_date",)
+    # ordering = ['-created_date']
+    list_filter = ('email',)
+    search_fields = ['name','massage']
+    
+
+admin.site.register(Contact,ContactAdmin)
+
