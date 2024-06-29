635bbafa2c1e515e1daba723f1fd1e3c40927320
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-06-29 18:11:10 +0330

    show data in  with  test.html file

--- django/MYSITE/mysite/blog/views.py
+++ django/MYSITE/mysite/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from blog.models import Post 
 # Create your views here
 def blog_view (request):
     return render(request, 'blog/blog-home.html')
@@ -7,3 +7,8 @@ def blog_view (request):
 def blog_single(request):
     context = {'title':'Bitcoin crashed againd','content':'Bitcoin was finally recovered','author':'amirabbas'}
     return render(request, 'blog/blog-single.html',context)
+
+def blog_test(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render (request,'test.html',context)
