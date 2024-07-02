5c432c198fd4833b6227d4b2b6e4832f2f792a34
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-07-02 13:11:32 +0330

    work with category

--- django/MYSITE/mysite/blog/models.py
+++ django/MYSITE/mysite/blog/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+class category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) :
+        return self.name
 class Post(models.Model):
     image = models.ImageField(upload_to='blog/',default='blog/default.jpg')
     author = models.ForeignKey(User, related_name = "author" , on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
     #tags
-    #category
+    category  = models.ManyToManyField(category, blank=True,)
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField( default= False)
     published_date = models.DateTimeField(null= True)
@@ -19,3 +25,4 @@ class Post(models.Model):
         verbose_name_plural = ' پست ها'
     def __str__(self):
         return" {}" .format(self.title ,)
+
