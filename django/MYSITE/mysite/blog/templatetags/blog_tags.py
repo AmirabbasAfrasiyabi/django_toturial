c90f26fca3289155f3f42cefa48b0df5eb75730f
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-07-03 17:00:57 +0330

    work with template filter

--- django/MYSITE/mysite/blog/templatetags/blog_tags.py
+++ django/MYSITE/mysite/blog/templatetags/blog_tags.py
@@ -7,4 +7,11 @@ def function():
     posts = Post.objects.filter(status = 1).count()
     return posts
 
-# 
+@register.simple_tag(name = 'posts')
+def function():
+    posts = Post.objects.filter(status = 1)
+    return posts
+
+@register.filter
+def snippet(value,arg=20):
+    return value[:arg] + "..."
