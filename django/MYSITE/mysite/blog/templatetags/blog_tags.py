78c8e1200baf54d3fbe400b2406d175c18555ab8
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-07-04 18:24:40 +0330

    delete categoru item

--- django/MYSITE/mysite/blog/templatetags/blog_tags.py
+++ django/MYSITE/mysite/blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post
+from blog.models import category
 register = template.Library()
 
 @register.simple_tag(name = 'totalpost')
@@ -20,3 +21,13 @@ def snippet(value,arg=20):
 def latestposts(arg=3):
     posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
     return {'posts': posts}
+
+@ register.inclusion_tag('blog/blog-postCategory.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'categories': cat_dict}
+
