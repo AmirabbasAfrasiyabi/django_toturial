c817449386d6408a57b7a23ec28be9e165a11833
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-07-05 12:43:28 +0330

    updated

--- django/MYSITE/mysite/website/forms.py
+++ django/MYSITE/mysite/website/forms.py
@@ -5,4 +5,4 @@ class NameForm(forms.Form):
     name = forms.CharField(max_length=255)
     email = forms.EmailField(max_length=255)
     subject = forms.CharField(max_length=255)
-    massage = forms.CharField(widget=forms.Textarea)
+    massage = forms.CharField(widget=forms.Textarea)   
