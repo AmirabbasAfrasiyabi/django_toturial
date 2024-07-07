ccc1a77a042aab80f6e0dce23ea021a7fab7f5c3
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-07-07 22:40:00 +0330

    final updated

--- django/MYSITE/mysite/blog/templatetags/blog_tags.py
+++ django/MYSITE/mysite/blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post
+from blog.models import category
 register = template.Library()
 
 
@@ -14,7 +15,17 @@ def function():
 def snippet(value,arg=20):
     return value[:arg]+ '....'
 
-@ register.inclusion_tag('popularpost.html')
-def popularpost():
+@ register.inclusion_tag('blog/blog-popularpost.html')
+def latestposts():
     posts = Post.objects.filter(status=1).order_by('published_date')
     return{'posts': posts}
+
+@ register.inclusion_tag('blog/blog-post-category.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = category.objects.all()
+    cat_dict = {}
+    for name in categories:
+       cat_dict[name] =  posts.filter(category=name).count()
+
+    return{'categories': cat_dict}
