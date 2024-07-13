9d4ae1d32d94a8e37656e90b61882245eabd1341
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-07-13 19:29:13 +0330

    final updated

--- django/MYSITE/mysite/website/views.py
+++ django/MYSITE/mysite/website/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse , JsonResponse , HttpResponseRedirect
 from website.models import Contact
 from website.forms import NameForm , ContactForm ,  NewsletterForm
+from django.contrib import messages
 def index_view(request):
     return render(request,'website/index.html')
 
@@ -13,9 +14,9 @@ def contact_view(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponse('done')
+            messages.add_message(request,messages.SUCCESS , 'your ticket submited seccessfuly')
         else:
-            return HttpResponse('not valid')
+            messages.add_message(request,messages.ERROR , 'your ticket  didnt submited seccessfuly')
     form = ContactForm()     
     return render(request,'website/contact.html',{'form': form})
 
