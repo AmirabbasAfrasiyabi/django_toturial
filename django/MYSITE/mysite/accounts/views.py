f41cd7cd43032e14540106b2ba27ba1cb0aaad49
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-08-11 17:38:38 +0330

    login page updated to dont logs two user

--- django/MYSITE/mysite/accounts/views.py
+++ django/MYSITE/mysite/accounts/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate, login
 
 def login_view(request):
-    if request.method == "POST":
-        print(request.POST)
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-    return render(request, 'accounts/login.html')
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            print(request.POST)
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+        return render(request, 'accounts/login.html')
+    else:
+        return redirect('/')
 
 
 # def logout_view(request):
