082819ca02513dd1ef8063bb28cc045736d5cf8c
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-12-15 15:54:25 +0330

    Prerequisite for implementing the project is on the host.

--- django/MYSITE/mysite/mysite/settings.py
+++ django/MYSITE/mysite/mysite/settings.py
@@ -16,22 +16,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-67taecvhtx6(d0!0c#*&=zc0ka#bi18o31of)v*sv3s1v1fc&o'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
-#site freamwork
-SITE_ID = 2
 
-CSRF_COOKIE_SECURE = False  # برای محیط توسعه
+
+CSRF_COOKIE_SECURE = False  
 CSRF_USE_SESSIONS = True
 
 
@@ -126,15 +115,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -172,15 +153,8 @@ APPEND_SLASH = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "statics",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -192,4 +166,3 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-X_FRAME_OPTIONS = "SAMEORIGIN"
